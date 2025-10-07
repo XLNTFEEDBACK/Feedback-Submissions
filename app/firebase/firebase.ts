7de143499421b6a9f9c3c25f0c17dfac235196f4
Kian Fuertes <kian@Kians-MBP.lan>
2025-10-07 15:21:19 -0700

    Use environment variables for Firebase config

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2iBn1AU0DprQezc_w1b6IalPiLUsJjtc",
-  authDomain: "xlnt-feedback-submission-b61b0.firebaseapp.com",
-  projectId: "xlnt-feedback-submission-b61b0",
-  storageBucket: "xlnt-feedback-submission-b61b0.firebasestorage.app",
-  messagingSenderId: "197514562557",
-  appId: "1:197514562557:web:8085dfadee66920515a794"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const db = getFirestore(app);
 
-export { db };
