c1a61fd21fa8ad1dc17ab2d186020872890b60b2
Kian Fuertes <kian@Kians-MBP.lan>
2025-10-07 15:15:07 -0700

    Fix import path for Firebase on Vercel

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { db } from "@/firebase/firebase";
+import { db } from "../../firebase/firebase";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 export async function POST(req: NextRequest) {
